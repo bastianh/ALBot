8c4971118c79cc1f7b57755242f6461147723049
Bastian Hoyer <dafire@gmail.com>
2020-06-29 23:01:51 +0200

    update

--- main.js
+++ main.js
@@ -11,9 +11,11 @@ const fs = require('fs');
 var bots = {}
 var inactiveBots = {};
 var httpWrapper = undefined;
+var activeChildren = {};
+var codeStatus = {};
+var config = {};
 
 const io = require('socket.io-client');
-
 
 async function main() {
     const token = process.env.TOKEN;
@@ -26,10 +28,13 @@ async function main() {
     }
 
     const url = `https://${host}/api/code/cli.js?token=${token}`
+    console.log(`fetching script ${url}`)
     const code = await request.get(url)
     fs.writeFileSync("CODE/default.js", code)
 
-    const socket = io(`https://${socket_host}/albot?token=${token}`,
+    const socket_url = `https://${socket_host}/albot?token=${token}`
+    console.log("connecting socket:" + socket_url)
+    const socket = io(socket_url,
         {transport: ['websocket']})
     socket.on('connect', (d) => {
         console.log("socket.io connected", socket_host);
@@ -44,37 +49,56 @@ async function main() {
     });
 
     socket.on('start_character', (arg1) => {
-        if (bots[arg1.char_name]) return;
+        if (bots[arg1.char_name] && bots[arg1.char_name].status !== "off") return;
         if (!httpWrapper) {
             httpWrapper = new HttpWrapper(arg1.auth, arg1.auth.split("-")[1], arg1.auth.split("-")[0]);
             updateCharacters(httpWrapper)
             setInterval(updateCharacters, 20000, httpWrapper);
         }
-        const args = [arg1.auth, arg1.auth.split("-")[1], arg1.auth.split("-")[0], arg1.ip, arg1.port, arg1.char_id, "default.js", false];
-        startGame(args)
-        bots[arg1.char_name] = {enabled: true, name: arg1.char_name,}
-        console.log(bots);
-        socket.emit("info", {bots})
+        config['auth'] = arg1.auth;
+
+        bots[arg1.char_name] = {
+            status: "start",
+            charName: arg1.char_name,
+            ip: arg1.ip,
+            port: arg1.port,
+            charId: arg1.char_id
+        }
     });
 
     socket.on('stop_character', (arg1) => {
-        if (!bots[arg1.char_name]) return;
-        if (!activeChildren[arg1.char_name]) return;
-        activeChildren[arg1.char_name].kill();
-
-        delete activeChildren[arg1.char_name];
-        delete bots[arg1.char_name]
-        console.log(bots);
-        socket.emit("info", {bots})
+        if (!bots[arg1.char_name] || bots[arg1.char_name].status === "off") return;
+        bots[arg1.char_name].stop = true;
     });
 
     socket.on('reload', (data) => {
         socket.emit("info", {bots})
     })
+
+    setInterval(() => {
+        console.log("----");
+        for (const bot of Object.values(bots)) {
+            if (bot.stop) {
+                if (activeChildren[bot.charName]) {
+                    activeChildren[bot.charName].kill();
+                    delete activeChildren[bot.charName];
+                    delete bot.stop
+                    bot.status = "off"
+                }
+            }
+            switch (bot.status) {
+                case 'start':
+                    bot.status = "loading"
+                    console.log("starting bot...")
+                    startGame(bot.charName, [config.auth, config.auth.split("-")[1], config.auth.split("-")[0], bot.ip, bot.port, bot.charId, "default.js", false]);
+                    break
+            }
+            console.log(bot.charName, bot.status)
+        }
+        socket.emit("info", {bots})
+    }, 1000)
 }
 
-var activeChildren = {};
-var codeStatus = {};
 
 function updateChildrenData() {
     for (var i in activeChildren) {
@@ -100,7 +124,7 @@ async function updateCharacters(httpWrapper) {
     }
 }
 
-function startGame(args) {
+function startGame(charName, args) {
     let childProcess = child_process.fork("./app/game", args, {
         stdio: [0, 1, 2, 'ipc'],
         execArgv: [
@@ -111,7 +135,7 @@ function startGame(args) {
     });
 
     childProcess.on('message', (m) => {
-        // console.log("MESSAGE INFO", m);
+        console.log("MESSAGE INFO", m);
         if (m.type === "status" && m.status === "disconnected") {
             childProcess.kill();
             for (var i in activeChildren) {
@@ -121,17 +145,18 @@ function startGame(args) {
                 }
             }
             // BotWebInterface.SocketServer.getPublisher().removeInterface(botInterface);
-            startGame(args);
+            startGame(charName, args);
             updateChildrenData();
         } else if (m.type === "bwiUpdate") {
-            // data = m.data;
+            bots[charName].bwi = m.data;
         } else if (m.type === "bwiPush") {
             // botInterface.pushData(m.name, m.data);
         } else if (m.type === "startupClient") {
             activeChildren[m.characterName] = childProcess;
+            bots[m.characterName].status = "running"
+            bots[m.characterName].lastConnect = new Date().getTime();
             codeStatus[m.characterName] = "code";
             updateChildrenData();
-            //socket.emit("info", {bots})
         } else if (m.type === "send_cm") {
             if (activeChildren[m.characterName]) {
                 activeChildren[m.characterName].send({
@@ -147,14 +172,15 @@ function startGame(args) {
                 });
             }
         } else if (m.type === "start_character") {
-            let bot = inactiveBots[m.name];
-            if (bot && !activeChildren[m.name]) {
-                codeStatus[m.name] = "loading";
-                if (m.data && inactiveBots[m.name]) inactiveBots[m.name][6] = m.data + ".js";
-                startGame(bot);
-                updateChildrenData()
-                console.log("started", m.name)
-            }
+            /*            let bot = inactiveBots[m.name];
+                        if (bot && !activeChildren[m.name]) {
+                            codeStatus[m.name] = "loading";
+                            if (m.data && inactiveBots[m.name]) inactiveBots[m.name][6] = m.data + ".js";
+                            startGame(bot);
+                            updateChildrenData()
+                            console.log("started", m.name)
+                        }
+             */
         }
     });
 }
